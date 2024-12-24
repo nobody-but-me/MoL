78c9b6b323a66cee6e72ecafb6393cfd83cb4865
boring-nerd-dot-py <nunesteixeiradaniel12@gmail.com>
2024-12-24 18:53:46 -0300

    MoL's dawn

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "[CONFIG] Do you want to run the project after this compilateion? "
+read -rep $"[CONFIG] (y or n): " _run
+
+
+_CC=gcc
+_FLAGS=-lglfw
+_SOURCES=./src/*.c
+
+$_CC -o ./build/MoL $_SOURCES -I./include/ $_FLAGS -Werror -Wall
+
+if [ $_run == "y" ]; then
+    echo "[INFO] Compiled Successfully. "
+    echo -e "[INFO] Running... \n"
+    ./run.sh
+else
+    echo "[INFO] Compiled without running application. "
+fi
---
+++ src/main.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+
+
+int main(int argc, char **argv) {
+  printf("[MOL] Hello, MoL! \n");
+  return 0;
+}
